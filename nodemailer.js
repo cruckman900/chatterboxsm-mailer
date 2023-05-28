2f22feb7f68c2617243f2bead269ff0b67dfb021
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2023-05-27 22:30:30 -0400

    hoping this works

--- nodemailer.js
+++ nodemailer.js
@@ -1,6 +1,19 @@
 require('dotenv').config({ debug: false });
 
+const ejs = require('ejs');
+
+console.log('First parameter: ', process.argv[2]);
+console.log('Second parameter: ', process.argv[3]);
+
+const express = require('express');
+var app = express();
+
 const nodemailer = require('nodemailer');
+var EmailTemplate = require('email-templates');
+
+let template = null;
+let userName = null;
+let verificationCode = null;
 
 let transporter = nodemailer.createTransport({
     host: process.env.host,
@@ -12,19 +25,34 @@ let transporter = nodemailer.createTransport({
     }
 });
 
-console.log('credentials', process.env.emailUser + ' ' + process.env.emailPass);
+app.get('/validateUser', function(req, res) {
+    template = "EmailTemplates/verifyuser.ejs";
+    email = req.query.e;
+    userName = req.query.un;
+    verificationCode = req.query.vc;
+})
 
-var mailOptions = {
-    from: 'donotreply@chatterboxsm.com',
-    to: 'cruckman900@gmail.com', 
-    subject: 'Please verify your ChatterboxSM account.',
-    html: '<h1>Booya!</h1>'
-};
-
-transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-        console.log(error);
+if ( template !== null && userName !== null )
+ejs.renderFile(template, { userName: userName, verificationCode: verificationCode }, function (err, data) {
+    if (err) {
+        console.log(err);
     } else {
-        console.log('Email sent: ' + info.response);
+        var mailOptions = {
+            from: 'donotreply@chatterboxsm.com',
+            to: email, 
+            subject: 'Please verify your ChatterboxSM account.',
+            html: data
+        };
+        console.log("html data =========>", mailOptions.html);
+        
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
     }
 });
+
+app.listen(process.env.port, () => console.log(`App listening on port ${process.env.port}`));
